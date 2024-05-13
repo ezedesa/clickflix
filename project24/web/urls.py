b6eec93df53e87e7a4d596e67f4b9dba6bd16dc6
Russo Trv <juanitrovato@gmail.com>
2024-05-13 20:38:01 -0300

    cambios en la view

--- project24/web/urls.py
+++ project24/web/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('ver_pelicula', views.ver_pelicula, name='ver_pelicula')
+    path('ver_pelicula/<str:id>', views.ver_pelicula, name='ver_pelicula')
 ]
 
