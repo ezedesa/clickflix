18c9705ef57bbf4cd21bd73a6786b32948f6b506
ezedesa <ezequiel.194300@gmail.com>
2024-05-14 15:52:55 -0300

    validacion_compra

--- project24/web/urls.py
+++ project24/web/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('catalogo/',views.mostrar_catalogo, name="catalogo"),
-    path('ver_pelicula/<str:id>', views.ver_pelicula, name='ver_pelicula')
+    path('ver_pelicula/<str:id>', views.ver_pelicula, name='ver_pelicula'),
+    path('validacion_compra', views.validacion_compra, name="validacion_compra")
 ]
